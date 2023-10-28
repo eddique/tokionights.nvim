fff91a71d572f77b37e91576d78e2d6c7a7be4b7
Eric R <eric.rodriguez@gusto.com>
2023-10-28 14:42:23 -0400

    debug

--- lua/eddique/lazy.lua
+++ lua/eddique/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "eddique.plugins" } }, {
+require("lazy").setup({ { import = "eddique.plugins" }, { import = "eddique.plugins.lsp" } }, {
   install = {
     colorscheme = { "tokyonight" },
   },
