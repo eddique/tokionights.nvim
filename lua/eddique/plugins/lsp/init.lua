dedcbadf6bbeb351c75bce6a083e5d3ecd4bc12f
Eric R <eric.rodriguez@gusto.com>
2023-10-29 16:36:59 -0400

    debug

--- lua/eddique/plugins/lsp/init.lua
+++ lua/eddique/plugins/lsp/init.lua
@@ -1,5 +1,6 @@
-return {
-    require("eddique.plugins.lsp.lspconfig"),
-    require("eddique.plugins.lsp.mason"),
-    require("eddique.plugins.lsp.none-ls")
-}
+-- return {
+--     require("eddique.plugins.lsp.lspconfig"),
+--     require("eddique.plugins.lsp.mason"),
+--     require("eddique.plugins.lsp.none-ls")
+-- }
+require('eddique.plugins.lsp.lspconfig')
